24e7651dd2c8cb26ce1a255a0371efbf308afcf8
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2025-10-25 13:24:31 -0400

    feat: progress on install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,22 +1,95 @@
 #!/usr/bin/env bash
 
-declare -r script_directory="$(dirname "$(realpath "$0")")"
+declare -r script_path="$(realpath "$0")"
+declare -r script_name="$(basename --suffix=.sh "$script_path")"
+declare -r script_directory="$(dirname "$script_path")"
+declare -r script_version="0.1.0"
 
 if ! source "$script_directory/utility.sh"; then
-    echo 'Error: failed to source utility script'
+    echo -e '\e[90m(0.000000000s) \e[31mError:\e[0m failed to source utility script'
 
     exit 1
 fi
 
-while getopts 'h' argument; do
-    case $argument in
-    h)
-        echo 'no way'
+function echo_usage() {
+    local indent="  "
+
+    echo -e "$(style bold)$script_name v$script_version$(style reset)"
+    echo -e "${indent}$(style italic)Applies this repository's configuration to your system$(style reset)"
+    echo
+    echo -e "$(style bold)Usage:$(style reset) ${script_name}.sh [OPTION]..."
+    echo
+    echo -e "$(style bold)Options:$(style reset)"
+    echo -e "${indent}${indent}${indent}--debug\t\t\tEnable debug logging for the current run"
+    echo
+    echo -e "${indent}-h, --help\t\t\tPrint the script's usage and exit"
+    echo -e "${indent}-V, --version\t\t\tPrint the script's version and exit"
+}
+
+while [[ -n "${1:-}" && "$1" =~ ^- && ! "$1" == "--" ]]; do
+    echo_debug "visiting argument '$1'"
+
+    case "$1" in
+    --debug)
+        debug_set_enabled 1
+        debug_flush_queue
+        ;;
+    -h | --help)
+        echo_usage
+        exit 0
+        ;;
+    -V | --version)
+        echo "$script_name v$script_version"
         exit 0
         ;;
     *)
-        echo 'what'
-        exit 2
+        echo_error "unknown argument '$1', use '--help' for a list of valid options"
         ;;
     esac
+
+    shift 1
 done
+
+unset print_help
+
+function directory_define() {
+    local array_name="directory_array_$1"
+
+    if is_undeclared "$array_name"; then
+        declare -gA $array_name
+    fi
+
+    if [ -n "${3:-}" ]; then
+        echo_debug "setting directory '$1/$2' to '$3'"
+    elif [ -n "${4:-}" ]; then
+        echo_debug "setting directory '$1/$2' to fallback '$4'"
+    else
+        echo_warn "no value provided to directory '$1/$2'"
+
+        return 1
+    fi
+
+    eval "$array_name[$2]=\"${3:-"${4:-}"}\""
+}
+
+function directory() {
+    local array_name="directory_array_$1"
+
+    if is_undeclared "$array_name"; then
+        echo_error "unknown directory category '$1'"
+    fi
+
+    local value="$(eval "echo \"\${$array_name[$2]}\"")"
+
+    if [ -n "$value" ]; then
+        echo -n "$value${3:-}"
+    else
+        echo_error "unknown directory '$1/$2'"
+    fi
+}
+
+directory_define 'system' 'cache' "${XDG_CACHE_HOME:-}" "$HOME/.cache"
+directory_define 'system' 'config' "${XDG_CONFIG_HOME:-}" "$HOME/.config"
+directory_define 'system' 'data' "${XDG_DATA_HOME:-}" "$HOME/.local/share"
+directory_define 'system' 'state' "${XDG_STATE_HOME:-}" "$HOME/.local/state"
+directory_define 'config' 'nvim' "$(directory system config '/.nvim')"
--- scripts/utility.sh
+++ scripts/utility.sh
@@ -1,2 +1,105 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+declare -r start_time="$(date +%s.%N)"
+
+function time_elapsed() {
+    echo "$(date +%s.%N --date="$start_time seconds ago")"
+}
+
+function is_declared() {
+    [[ -n "${!1+waow}" ]] || declare -p $1 &>/dev/null
+}
+
+function is_undeclared() {
+    [[ -z "${!1+waow}" ]] && ! declare -p $1 &>/dev/null
+}
+
+declare -A style_array=()
+
+function style_define() {
+    style_array["$1"]="\e[${2}m"
+
+    if [ -n "${3:-}" ]; then
+        style_array["${1}-background"]="\e[${3}m"
+    fi
+    if [ -n "${4:-}" ]; then
+        style_array["bright-${1}"]="\e[${4}m"
+    fi
+    if [ -n "${5:-}" ]; then
+        style_array["bright-${1}-background"]="\e[${5}m"
+    fi
+}
+
+style_define reset 0
+style_define bold 1
+style_define italic 3
+style_define underline 4
+style_define strikethrough 9
+style_define black 30 40 90 100
+style_define red 31 41 91 101
+style_define green 32 42 92 102
+style_define yellow 33 43 93 103
+style_define blue 34 44 94 104
+style_define magenta 35 45 95 105
+style_define cyan 36 46 96 106
+style_define white 37 47 97 107
+
+function style() {
+    for style_key in "$@"; do
+        local style_string="${style_array["$style_key"]}"
+
+        if [ -n "$style_string" ]; then
+            echo -en "$style_string"
+        else
+            echo_error "invalid style '$1'"
+        fi
+    done
+
+    unset style_key
+}
+
+declare -i debug_enabled=0
+declare -a debug_queued=()
+
+function debug_set_enabled() {
+    if [[ "$debug_enabled" -ne "$1" ]]; then
+        debug_enabled=$1
+    else
+        echo_debug "debug flag has already been set to '$1'"
+    fi
+}
+
+function debug_flush_queue() {
+    if [ $debug_enabled -eq 1 ]; then
+        for queued in "${debug_queued[@]}"; do
+            echo "$queued"
+        done
+
+        debug_queued=()
+    else
+        echo_warn 'attempted to flush debugging queue while debugging is disabled'
+    fi
+}
+
+function echo_debug() {
+    if [ $debug_enabled -eq 1 ] || [ "${2:-0}" == 1 ]; then
+        >&2 echo -e "$(style bright-black)($(time_elapsed)s) $(style bright-magenta)Debug:$(style reset) $1"
+    else
+        debug_queued+=("$(echo_debug "$1" 1 2>&1)")
+    fi
+}
+
+function echo_info() {
+    echo -e "$(style bright-black)($(time_elapsed)s) $(style blue)Info:$(style reset) $1"
+}
+
+function echo_warn() {
+    >&2 echo -e "$(style bright-black)($(time_elapsed)s) $(style yellow)Warning:$(style reset) $1"
+}
+
+function echo_error() {
+    >&2 echo -e "$(style bright-black)($(time_elapsed)s) $(style red)Error:$(style reset) $1"
+    exit "${2:-1}"
+}
