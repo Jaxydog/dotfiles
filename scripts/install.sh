1d736917c0b8f76843b301893048d82075410c4e
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2025-10-29 17:56:59 -0400

    feat(scripts): major script refactor, improve code usability and error handling

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,90 +6,118 @@ declare -r script_directory="$(dirname "$script_path")"
 declare -r script_version="0.1.0"
 
 if ! source "$script_directory/utility.sh"; then
-    echo -e '\e[90m(0.000000000s) \e[31mError:\e[0m failed to source utility script'
-
+    echo 'Failed to source utility script'
     exit 1
 fi
 
-function echo_usage() {
-    local indent="  "
+log debug 'checking installed system'
 
-    echo -e "$(style bold)$script_name v$script_version$(style reset)"
-    echo -e "${indent}$(style italic)Applies this repository's configuration to your system$(style reset)"
+if [ ! -f '/etc/arch-release' ] && ! uname -r | grep --quiet --ignore-case 'arch'; then
+    log error 'this script should only be run on Arch linux'
+fi
+
+function echo_usage() {
+    local indent='  '
+
     echo
-    echo -e "$(style bold)Usage:$(style reset) ${script_name}.sh [OPTION]..."
+    echo \
+        "$(style get bold fg-bright-cyan)${script_name}$(style get reset)" \
+        "$(style get fg-white)v${script_version}$(style get reset)"
+    echo \
+        "$(style get italic)${indent}Applies the files within this repository to your system$(style get reset)"
     echo
-    echo -e "$(style bold)Options:$(style reset)"
-    echo -e "${indent}${indent}${indent}--debug\t\t\tEnable debug logging for the current run"
+    echo \
+        "$(style get bold fg-bright-green)Usage:$(style get reset)" \
+        "$(style get fg-bright-cyan)${script_name}.sh$(style get reset)" \
+        "$(style get fg-cyan)[OPTION]...$(style get reset)"
     echo
-    echo -e "${indent}-h, --help\t\t\tPrint the script's usage and exit"
-    echo -e "${indent}-V, --version\t\t\tPrint the script's version and exit"
+    echo \
+        "$(style get bold fg-bright-green)Options:$(style get reset)"
+    echo -e \
+        "$(style get fg-bright-cyan)${indent}-n$(style get fg-white)," \
+        "$(style get fg-bright-cyan)--dry-run$(style get reset)" \
+        "$(style get italic)\t\tRun the script without modifying the system$(style get reset)"
+    echo -e \
+        "$(style get fg-bright-cyan)${indent}-z$(style get fg-white)," \
+        "$(style get fg-bright-cyan)--enable-debug$(style get reset)" \
+        "$(style get italic)\t\tEnable debug logging for the current run$(style get reset)"
+    echo
+    echo -e \
+        "$(style get fg-bright-cyan)${indent}-h$(style get fg-white)," \
+        "$(style get fg-bright-cyan)--help$(style get reset)" \
+        "$(style get italic)\t\t\tPrint the script's usage and exit$(style get reset)"
+    echo -e \
+        "$(style get fg-bright-cyan)${indent}-V$(style get fg-white)," \
+        "$(style get fg-bright-cyan)--version$(style get reset)" \
+        "$(style get italic)\t\tPrint the script's version and exit$(style get reset)"
+    echo
 }
 
-while [[ -n "${1:-}" && "$1" =~ ^- && ! "$1" == "--" ]]; do
-    echo_debug "visiting argument '$1'"
+for ((index = 1; index <= "$#"; index += 1)); do
+    argument="${!index:-}"
 
-    case "$1" in
-    --debug)
-        debug_set_enabled 1
-        debug_flush_queue
+    if [[ "$argument" =~ ^- ]]; then
+        log debug "visiting command-line argument '$argument'"
+    else
+        log debug "encountered positional argument '$argument', cancelling argument parse loop"
+
+        break 1
+    fi
+
+    case "$argument" in
+    '-z' | '--debug-enabled')
+        log debug 'enabling debug logging'
+        log set debug-enabled 1
         ;;
-    -h | --help)
+    '-n' | '--dry-run')
+        log debug 'enabling dry run, system will not be modified'
+        call set dry-run 1
+        ;;
+    '-h' | '--help')
         echo_usage
         exit 0
         ;;
-    -V | --version)
+    '-V' | '--version')
         echo "$script_name v$script_version"
         exit 0
         ;;
+    '--')
+        log debug 'encountered positional separator, cancelling argument parse loop'
+        index="$((index + 1))"
+        break 1
+        ;;
+    '')
+        log error "$(scriptmsg 'missing-argument' "$index")"
+        exit 2
+        ;;
     *)
-        echo_error "unknown argument '$1', use '--help' for a list of valid options"
+        log error "$(scriptmsg 'invalid-argument' "$index" "$argument")"
+        exit 2
         ;;
     esac
 
-    shift 1
+    unset argument
 done
+
+declare -a script_positionals=()
+
+for (( ; index <= "$#"; index += 1)); do
+    script_positionals+=("${!index:-}")
+done
+
+if [ "${#script_positionals[@]}" -gt 0 ]; then
+    log error 'positional arguments are not supported'
+    exit 2
+fi
 
 unset print_help
 
-function directory_define() {
-    local array_name="directory_array_$1"
+directory set config nvim "$(directory get system config)/nvim"
 
-    if is_undeclared "$array_name"; then
-        declare -gA $array_name
-    fi
+log info 'checking for system updates'
 
-    if [ -n "${3:-}" ]; then
-        echo_debug "setting directory '$1/$2' to '$3'"
-    elif [ -n "${4:-}" ]; then
-        echo_debug "setting directory '$1/$2' to fallback '$4'"
-    else
-        echo_warn "no value provided to directory '$1/$2'"
+call group open 'system-update'
+call eval sudo pacman -Syu
+call group close
 
-        return 1
-    fi
-
-    eval "$array_name[$2]=\"${3:-"${4:-}"}\""
-}
-
-function directory() {
-    local array_name="directory_array_$1"
-
-    if is_undeclared "$array_name"; then
-        echo_error "unknown directory category '$1'"
-    fi
-
-    local value="$(eval "echo \"\${$array_name[$2]}\"")"
-
-    if [ -n "$value" ]; then
-        echo -n "$value${3:-}"
-    else
-        echo_error "unknown directory '$1/$2'"
-    fi
-}
-
-directory_define 'system' 'cache' "${XDG_CACHE_HOME:-}" "$HOME/.cache"
-directory_define 'system' 'config' "${XDG_CONFIG_HOME:-}" "$HOME/.config"
-directory_define 'system' 'data' "${XDG_DATA_HOME:-}" "$HOME/.local/share"
-directory_define 'system' 'state' "${XDG_STATE_HOME:-}" "$HOME/.local/state"
-directory_define 'config' 'nvim' "$(directory system config '/.nvim')"
+call group require closed
--- scripts/utility.sh
+++ scripts/utility.sh
@@ -2,104 +2,595 @@
 
 set -euo pipefail
 
-declare -r start_time="$(date +%s.%N)"
+function scriptmsg() {
+    local argument="${1:-}"
 
-function time_elapsed() {
-    echo "$(date +%s.%N --date="$start_time seconds ago")"
+    case "$argument" in
+    'missing-argument')
+        echo "missing argument #${2:-1} for function '${FUNCNAME[1]}' (called from '${FUNCNAME[2]:-<?>}')"
+        ;;
+    'invalid-argument')
+        echo "invalid argument #${2:-1} '${3:-?}' for function '${FUNCNAME[1]}' (called from '${FUNCNAME[2]:-<?>}')"
+        ;;
+    '')
+        log error "$(scriptmsg 'missing-argument' 1)"
+        return 1
+        ;;
+    *)
+        log error "$(scriptmsg 'invalid-argument' 1 "$argument")"
+        return 1
+        ;;
+    esac
 }
 
-function is_declared() {
-    [[ -n "${!1+waow}" ]] || declare -p $1 &>/dev/null
+function var() {
+    local argument="${1:-}"
+
+    case "$argument" in
+    'assigned')
+        [[ -n "${!2+waow}" ]] || declare -p "$2" &>/dev/null
+        ;;
+    'unassigned')
+        [[ -z "${!2+waow}" ]] && ! declare -p "$2" &>/dev/null
+        ;;
+    '')
+        log error "$(scriptmsg 'missing-argument' 1)"
+        return 1
+        ;;
+    *)
+        log error "$(scriptmsg 'invalid-argument' 1 "$argument")"
+        return 1
+        ;;
+    esac
 }
 
-function is_undeclared() {
-    [[ -z "${!1+waow}" ]] && ! declare -p $1 &>/dev/null
-}
-
-declare -A style_array=()
-
-function style_define() {
-    style_array["$1"]="\e[${2}m"
-
-    if [ -n "${3:-}" ]; then
-        style_array["${1}-background"]="\e[${3}m"
-    fi
-    if [ -n "${4:-}" ]; then
-        style_array["bright-${1}"]="\e[${4}m"
-    fi
-    if [ -n "${5:-}" ]; then
-        style_array["bright-${1}-background"]="\e[${5}m"
-    fi
-}
-
-style_define reset 0
-style_define bold 1
-style_define italic 3
-style_define underline 4
-style_define strikethrough 9
-style_define black 30 40 90 100
-style_define red 31 41 91 101
-style_define green 32 42 92 102
-style_define yellow 33 43 93 103
-style_define blue 34 44 94 104
-style_define magenta 35 45 95 105
-style_define cyan 36 46 96 106
-style_define white 37 47 97 107
+declare -A SCRIPT_STYLE_MAP=()
+declare -i SCRIPT_STYLE_INITIALIZED=0
 
 function style() {
-    for style_key in "$@"; do
-        local style_string="${style_array["$style_key"]}"
+    local argument="${1:-}"
 
-        if [ -n "$style_string" ]; then
-            echo -en "$style_string"
-        else
-            echo_error "invalid style '$1'"
+    case "$argument" in
+    'get')
+        shift 1
+
+        if [ "$#" -eq 0 ]; then
+            log error "$(scriptmsg 'missing-argument' 2)"
+            return 1
         fi
-    done
 
-    unset style_key
-}
+        for key in "$@"; do
+            if [ -z "$key" ]; then
+                log error "$(scriptmsg 'invalid-argument' 2 '')"
+                return 1
+            fi
 
-declare -i debug_enabled=0
-declare -a debug_queued=()
+            local value="${SCRIPT_STYLE_MAP["$key"]:-}"
 
-function debug_set_enabled() {
-    if [[ "$debug_enabled" -ne "$1" ]]; then
-        debug_enabled=$1
-    else
-        echo_debug "debug flag has already been set to '$1'"
-    fi
-}
-
-function debug_flush_queue() {
-    if [ $debug_enabled -eq 1 ]; then
-        for queued in "${debug_queued[@]}"; do
-            echo "$queued"
+            if [ -n "$value" ]; then
+                echo -ne "$value"
+            else
+                log error "$(scriptmsg 'invalid-argument' 2 "$key")"
+                return 1
+            fi
         done
+        ;;
+    'set')
+        local key="${2:-}"
+        local value="${3:-}"
 
-        debug_queued=()
-    else
-        echo_warn 'attempted to flush debugging queue while debugging is disabled'
-    fi
+        if [ -z "$key" ]; then
+            log error "$(scriptmsg 'missing-argument' 2)"
+            return 1
+        fi
+        if [ -z "$value" ]; then
+            log error "$(scriptmsg 'missing-argument' 3)"
+            return 1
+        fi
+
+        if [ "$SCRIPT_STYLE_INITIALIZED" -eq 1 ]; then
+            log debug "setting style '$key' to ANSI code '$value'"
+        fi
+
+        SCRIPT_STYLE_MAP["$key"]="\e[${value}m"
+        ;;
+    '')
+        log error "$(scriptmsg 'missing-argument' 1)"
+        return 1
+        ;;
+    *)
+        log error "$(scriptmsg 'invalid-argument' 1 "$argument")"
+        return 1
+        ;;
+    esac
 }
 
-function echo_debug() {
-    if [ $debug_enabled -eq 1 ] || [ "${2:-0}" == 1 ]; then
-        >&2 echo -e "$(style bright-black)($(time_elapsed)s) $(style bright-magenta)Debug:$(style reset) $1"
-    else
-        debug_queued+=("$(echo_debug "$1" 1 2>&1)")
-    fi
+style set reset 0
+style set bold 1
+style set faint 2
+style set italic 3
+style set underline 4
+style set blink 5
+style set strike 9
+style set fg-black 30
+style set bg-black 40
+style set fg-bright-black 90
+style set bg-bright-black 100
+style set fg-red 31
+style set bg-red 41
+style set fg-bright-red 91
+style set bg-bright-red 101
+style set fg-green 32
+style set bg-green 42
+style set fg-bright-green 92
+style set bg-bright-green 102
+style set fg-yellow 33
+style set bg-yellow 43
+style set fg-bright-yellow 93
+style set bg-bright-yellow 103
+style set fg-blue 34
+style set bg-blue 44
+style set fg-bright-blue 94
+style set bg-bright-blue 104
+style set fg-magenta 35
+style set bg-magenta 45
+style set fg-bright-magenta 95
+style set bg-bright-magenta 105
+style set fg-cyan 36
+style set bg-cyan 46
+style set fg-bright-cyan 96
+style set bg-bright-cyan 106
+style set fg-white 37
+style set bg-white 47
+style set fg-bright-white 97
+style set bg-bright-white 107
+style set fg-reset 39
+style set bg-reset 49
+style set super 73
+style set sub 74
+
+SCRIPT_STYLE_INITIALIZED=1
+
+declare -r SCRIPT_TIMER_START="$(date +%s.%N)"
+
+function timer() {
+    local argument="${1:-}"
+
+    case "$argument" in
+    'start')
+        echo "$SCRIPT_TIMER_START"
+        ;;
+    'elapsed')
+        echo "$(date +%s.%N --date="$SCRIPT_TIMER_START seconds ago")"
+        ;;
+    '')
+        log error "$(scriptmsg 'missing-argument' 1)"
+        return 1
+        ;;
+    *)
+        log error "$(scriptmsg 'invalid-argument' 1 "$argument")"
+        return 1
+        ;;
+    esac
 }
 
-function echo_info() {
-    echo -e "$(style bright-black)($(time_elapsed)s) $(style blue)Info:$(style reset) $1"
+declare -a SCRIPT_LOG_DEBUG_QUEUE=()
+declare -i SCRIPT_LOG_INITIALIZED=0
+declare -A SCRIPT_LOG_SETTINGS=()
+
+function log() {
+    local argument="${1:-}"
+
+    case "$argument" in
+    'get')
+        local key="${2:-}"
+
+        if [ -z "$key" ]; then
+            log error "$(scriptmsg 'invalid-argument' 2 '')"
+            return 1
+        fi
+
+        local value="${SCRIPT_LOG_SETTINGS["$key"]:-}"
+
+        if [ -n "$value" ]; then
+            echo -ne "$value"
+        else
+            log error "$(scriptmsg 'invalid-argument' 2 "$key")"
+            return 1
+        fi
+        ;;
+    'set')
+        local key="${2:-}"
+        local value="${3:-}"
+
+        if [ -z "$key" ]; then
+            log error "$(scriptmsg 'missing-argument' 2)"
+            return 1
+        fi
+        if [ -z "$value" ]; then
+            log error "$(scriptmsg 'missing-argument' 3)"
+            return 1
+        fi
+
+        if [ "$SCRIPT_LOG_INITIALIZED" -eq 1 ]; then
+            log debug "setting logging option '$key' to value '$value'"
+        fi
+
+        SCRIPT_LOG_SETTINGS["$key"]="$value"
+
+        if [ "$key" == 'debug-enabled' ] && [ "$value" -eq 1 ]; then
+            for queued_log in "${SCRIPT_LOG_DEBUG_QUEUE[@]}"; do
+                >&2 echo "$queued_log"
+            done
+
+            SCRIPT_LOG_DEBUG_QUEUE=()
+        fi
+        ;;
+    'info')
+        shift 1
+
+        if [ "$#" -eq 0 ]; then
+            log error "$(scriptmsg 'missing-argument' 2)"
+            return 1
+        fi
+
+        echo -e \
+            "$(style get fg-bright-black)[$(timer elapsed)s]$(style get reset)" \
+            "$(style get fg-bright-blue)(info)$(style get reset)" \
+            $@
+        ;;
+    'warn')
+        shift 1
+
+        if [ "$#" -eq 0 ]; then
+            log error "$(scriptmsg 'missing-argument' 2)"
+            return 1
+        fi
+
+        >&2 echo -e \
+            "$(style get fg-bright-black)[$(timer elapsed)s]$(style get reset)" \
+            "$(style get fg-bright-yellow)(warn)$(style get reset)" \
+            $@
+        ;;
+    'error')
+        shift 1
+
+        if [ "$#" -eq 0 ]; then
+            log error "$(scriptmsg 'missing-argument' 2)"
+            return 1
+        fi
+
+        >&2 echo -e \
+            "$(style get fg-bright-black)[$(timer elapsed)s]$(style get reset)" \
+            "$(style get fg-bright-red)(error)$(style get reset)" \
+            $@
+        ;;
+    'debug')
+        shift 1
+
+        if [ "$#" -eq 0 ]; then
+            log error "$(scriptmsg 'missing-argument' 2)"
+            return 1
+        fi
+
+        if [ "${SCRIPT_LOG_FORCE_DEBUG:-0}" -eq 1 ] || [ "$(log get debug-enabled)" -eq 1 ]; then
+            echo -e \
+                "$(style get fg-bright-black)[$(timer elapsed)s]$(style get reset)" \
+                "$(style get fg-magenta)(debug)$(style get reset)" \
+                "$(style get fg-white)$@$(style get reset)"
+        else
+            SCRIPT_LOG_DEBUG_QUEUE+=("$(SCRIPT_LOG_FORCE_DEBUG=1 log debug "$@")")
+        fi
+        ;;
+    '')
+        log error "$(scriptmsg 'missing-argument' 1)"
+        return 1
+        ;;
+    *)
+        log error "$(scriptmsg 'invalid-argument' 1 "$argument")"
+        return 1
+        ;;
+    esac
 }
 
-function echo_warn() {
-    >&2 echo -e "$(style bright-black)($(time_elapsed)s) $(style yellow)Warning:$(style reset) $1"
+log set debug-enabled 0
+
+SCRIPT_LOG_INITIALIZED=1
+
+function directory() {
+    local argument="${1:-}"
+
+    case "$argument" in
+    'get')
+        local category="${2:-}"
+        local key="${3:-}"
+
+        if [ -z "$category" ]; then
+            log error "$(scriptmsg 'missing-argument' 2)"
+            return 1
+        fi
+        if [ -z "$key" ]; then
+            log error "$(scriptmsg 'missing-argument' 3)"
+            return 1
+        fi
+
+        local global_variable_name="SCRIPT_DIRECTORY_ARRAY_${category^^}"
+
+        if var unassigned "$global_variable_name"; then
+            log error "unknown directory category '$category'"
+            return 1
+        fi
+
+        local value="$(eval "echo \"\${$global_variable_name[$key]}\"")"
+
+        if [ -z "$value" ]; then
+            log error "unknown directory '$category/$key'"
+            return 1
+        fi
+
+        echo -n "$value"
+        ;;
+    'set')
+        local category="${2:-}"
+        local key="${3:-}"
+        local value="${4:-}"
+
+        if [ -z "$category" ]; then
+            log error "$(scriptmsg 'missing-argument' 2)"
+            return 1
+        fi
+        if [ -z "$key" ]; then
+            log error "$(scriptmsg 'missing-argument' 3)"
+            return 1
+        fi
+        if [ -z "$value" ]; then
+            log error "$(scriptmsg 'missing-argument' 4)"
+            return 1
+        else
+            value="$(realpath -ms "$value")"
+
+            case "$value" in
+            *[!/]*/)
+                value="${value%\"${x##*[!/]}}"
+                ;;
+            *[/])
+                value="/"
+                ;;
+            esac
+        fi
+
+        local global_variable_name="SCRIPT_DIRECTORY_ARRAY_${category^^}"
+
+        if var unassigned "$global_variable_name"; then
+            declare -gA "$global_variable_name"
+        fi
+
+        log debug "setting directory '$category/$key' to path '$value'"
+
+        eval "$global_variable_name[$key]='$value'"
+        ;;
+    '')
+        log error "$(scriptmsg 'missing-argument' 1)"
+        return 1
+        ;;
+    *)
+        log error "$(scriptmsg 'invalid-argument' 1 "$argument")"
+        return 1
+        ;;
+    esac
 }
 
-function echo_error() {
-    >&2 echo -e "$(style bright-black)($(time_elapsed)s) $(style red)Error:$(style reset) $1"
-    exit "${2:-1}"
+directory set system cache "${XDG_CACHE_HOME:-"$HOME/.cache"}"
+directory set system config "${XDG_CONFIG_HOME:-"$HOME/.config"}"
+directory set system data "${XDG_DATA_HOME:-"$HOME/.local/share"}"
+directory set system state "${XDG_STATE_HOME:-"$HOME/.local/state"}"
+
+declare -i SCRIPT_CALL_GROUP_CONTINUE=0
+declare -a SCRIPT_CALL_GROUP_HEIRARCHY=()
+declare -A SCRIPT_CALL_SETTINGS=()
+
+function call() {
+    local argument="${1:-}"
+
+    case "$argument" in
+    'get')
+        local key="${2:-}"
+
+        if [ -z "$key" ]; then
+            log error "$(scriptmsg 'invalid-argument' 2 '')"
+            return 1
+        fi
+
+        local value="${SCRIPT_CALL_SETTINGS["$key"]:-}"
+
+        if [ -n "$value" ]; then
+            echo -ne "$value"
+        else
+            log error "$(scriptmsg 'invalid-argument' 2 "$key")"
+            return 1
+        fi
+        ;;
+    'set')
+        local key="${2:-}"
+        local value="${3:-}"
+
+        if [ -z "$key" ]; then
+            log error "$(scriptmsg 'missing-argument' 2)"
+            return 1
+        fi
+        if [ -z "$value" ]; then
+            log error "$(scriptmsg 'missing-argument' 3)"
+            return 1
+        fi
+
+        log debug "setting call option '$key' to value '$value'"
+
+        SCRIPT_CALL_SETTINGS["$key"]="$value"
+        ;;
+    'group')
+        argument="${2:-}"
+
+        case "$argument" in
+        'require')
+            argument="${3:-}"
+
+            case "$argument" in
+            'opened')
+                if [ "${#SCRIPT_CALL_GROUP_HEIRARCHY[@]}" -eq 0 ]; then
+                    log error 'no call group is currently opened'
+                    return 1
+                fi
+
+                local group_name="${4:-}"
+
+                if [ -n "$group_name" ] && [ "${SCRIPT_CALL_GROUP_HEIRARCHY[-1]}" != "$group_name" ]; then
+                    log error "current group '${SCRIPT_CALL_GROUP_HEIRARCHY[-1]}' does not match '$group_name'"
+                    return 1
+                fi
+                ;;
+            'closed')
+                if [ "${#SCRIPT_CALL_GROUP_HEIRARCHY[@]}" -gt 0 ]; then
+                    log error "current group '${SCRIPT_CALL_GROUP_HEIRARCHY[-1]}' is still open"
+                    return 1
+                fi
+                ;;
+            '')
+                log error "$(scriptmsg 'missing-argument' 3)"
+                return 1
+                ;;
+            *)
+                log error "$(scriptmsg 'invalid-argument' 3 "$argument")"
+                return 1
+                ;;
+            esac
+            ;;
+        'open')
+            local value="${3:-}"
+
+            if [ -z "$value" ]; then
+                log error "$(scriptmsg 'missing-argument' 3)"
+                return 1
+            fi
+
+            log debug "creating new call group '$value'"
+
+            SCRIPT_CALL_GROUP_HEIRARCHY+=("$value")
+
+            if [ "$(call get print-group)" -eq 1 ]; then
+                local indent="$(
+                    indent="$(call get print-indent)"
+                    count="$(("${#SCRIPT_CALL_GROUP_HEIRARCHY[@]}" - 1))"
+
+                    for ((i = 0; i < "$count"; i++)); do echo -n "$indent"; done
+                )"
+
+                if [ "$SCRIPT_CALL_GROUP_CONTINUE" -eq 0 ]; then
+                    echo
+                fi
+
+                echo "$indent$(style get bold fg-bright-white)$value:$(style get reset)"
+            fi
+
+            SCRIPT_CALL_GROUP_CONTINUE=0
+            ;;
+        'close')
+            call group require opened
+
+            log debug "closing call group '${SCRIPT_CALL_GROUP_HEIRARCHY[-1]}'"
+
+            unset 'SCRIPT_CALL_GROUP_HEIRARCHY[${#SCRIPT_CALL_GROUP_HEIRARCHY[@]}-1]'
+
+            if [ "${#SCRIPT_CALL_GROUP_HEIRARCHY[@]}" -eq 0 ]; then
+                SCRIPT_CALL_GROUP_CONTINUE=0
+            fi
+            ;;
+        '')
+            log error "$(scriptmsg 'missing-argument' 2)"
+            return 1
+            ;;
+        *)
+            log error "$(scriptmsg 'invalid-argument' 2 "$argument")"
+            return 1
+            ;;
+        esac
+        ;;
+    'eval')
+        shift 1
+
+        if [ "${#SCRIPT_CALL_GROUP_HEIRARCHY[@]}" -gt 0 ] && [ "$SCRIPT_CALL_GROUP_CONTINUE" -eq 0 ]; then
+            echo
+            SCRIPT_CALL_GROUP_CONTINUE=1
+        fi
+
+        if [ "$(call get print-command)" -eq 1 ]; then
+            local indent="$(
+                indent="$(call get print-indent)"
+                count="$(("${#SCRIPT_CALL_GROUP_HEIRARCHY[@]}" - 1))"
+
+                for ((i = 0; i < "$count"; i++)); do echo -n "$indent"; done
+            )"
+
+            echo "$indent$(style get fg-white)> $@$(style get reset)"
+            echo
+        fi
+
+        local indent="$(
+            indent="$(call get print-indent)"
+            count="${#SCRIPT_CALL_GROUP_HEIRARCHY[@]}"
+
+            for ((i = 0; i < "$count"; i++)); do echo -n "$indent"; done
+        )"
+
+        if [ "$(call get dry-run)" -eq 1 ]; then
+            echo "$indent$(style get fg-bright-black italic)command disabled due to dry run"
+            echo
+        else
+            log debug "evaluating command '$@'"
+
+            local output
+            set +e
+            output=$(
+                > >(
+                    trap '' INT TERM
+                    while read line; do printf "$indent%s\n" "$line"; done
+                ) 2> >(
+                    trap '' INT TERM
+                    while read line; do >&2 printf "$indent%s\n" "$line"; done
+                ) $@ | tee -p --output-error=exit /dev/tty
+
+                exit_code="$?"
+                echo -e '.'
+                exit "$exit_code"
+            )
+
+            local exit_code="$?"
+            set -e
+
+            log debug "command exited with a value of '$exit_code'"
+
+            if [[ "${output%.}" != *$'\n\n' ]]; then
+                echo
+            fi
+
+            if [ "$exit_code" -ne 0 ]; then
+                log error "command failed with a code of '$exit_code'"
+
+                exit 1
+            fi
+        fi
+        ;;
+    '')
+        log error "$(scriptmsg 'missing-argument' 1)"
+        return 1
+        ;;
+    *)
+        log error "$(scriptmsg 'invalid-argument' 1 "$argument")"
+        return 1
+        ;;
+    esac
 }
+
+call set dry-run 0
+call set print-indent '  '
+call set print-group 1
+call set print-command 1
