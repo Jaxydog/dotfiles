df7108602161c2b9d76eeb8d1ef912afbb791b45
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2025-10-31 16:38:30 -0400

    feat(nvim): remove buffer provider for completions

--- configs/nvim/lua/plugin/blink-cmp.lua
+++ configs/nvim/lua/plugin/blink-cmp.lua
@@ -21,7 +21,7 @@ return {
                 },
             },
             sources = {
-                default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+                default = { 'lazydev', 'lsp', 'path', 'snippets', --[[ 'buffer' ]] },
                 providers = {
                     lazydev = { name = 'LazyDev', module = 'lazydev.integrations.blink', score_offset = 100 },
                 },
