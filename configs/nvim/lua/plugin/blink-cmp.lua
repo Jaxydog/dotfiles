93f862cddb4377284882f03b1e7a225dc75e11aa
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2025-11-25 17:46:57 -0500

    feat(nvim): re-enable buffer completion provider
    The provider will be used automatically when no other completions are available.
    This also removes some default snippets that I find annoying.

--- configs/nvim/lua/plugin/blink-cmp.lua
+++ configs/nvim/lua/plugin/blink-cmp.lua
@@ -1,3 +1,20 @@
+local ignored_snippet_labels = {
+    -- Default snippets that are always active and I will never use.
+    'copyright',
+    'date',
+    'dateDMY',
+    'dateMDY',
+    'datetime',
+    'diso',
+    'time',
+    'timeHMS',
+    'uuid',
+}
+local ignored_snippet_patterns = {
+    -- Added alongside other Rust snippets, and they're very big and annoying to scroll over. I also don't use Relm.
+    '^relm',
+}
+
 return {
     {
         'saghen/blink.cmp',
@@ -5,6 +22,7 @@ return {
         dependencies = {
             { 'rafamadriz/friendly-snippets' },
         },
+        ---@type blink.cmp.Config
         opts = {
             completion = {
                 ghost_text = {
@@ -58,10 +76,30 @@ return {
                 },
             },
             sources = {
-                default = { 'lazydev', 'lsp', 'path', 'snippets', --[[ 'buffer' ]] },
+                default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
                 providers = {
                     lazydev = { name = 'LazyDev', module = 'lazydev.integrations.blink', score_offset = 100 },
                 },
+                transform_items = function(_, items)
+                    local is_buffer_only = vim.tbl_isempty(vim.tbl_filter(function(item)
+                        ---@cast item blink.cmp.CompletionItem
+                        return item.source_name ~= 'buffer'
+                    end, items))
+
+                    if is_buffer_only then return items end
+
+                    return vim.tbl_filter(function(item)
+                        ---@cast item blink.cmp.CompletionItem
+
+                        return item.kind ~= require('blink.cmp.types').CompletionItemKind.Snippet
+                            or (
+                                not vim.tbl_contains(ignored_snippet_labels, item.label)
+                                and not vim.tbl_contains(ignored_snippet_patterns, function(pattern)
+                                    return item.label:match(pattern)
+                                end, { predicate = true })
+                            )
+                    end, items)
+                end
             },
         },
     },
