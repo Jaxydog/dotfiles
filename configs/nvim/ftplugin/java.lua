2b44fb07509a0f255e9695f8f4e676af183a2f96
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2026-01-11 17:32:18 -0500

    refactor(nvim): adjust keybinds

--- configs/nvim/ftplugin/java.lua
+++ configs/nvim/ftplugin/java.lua
@@ -16,16 +16,30 @@ require('jdtls').start_or_attach({
     },
 })
 
-local buffer = vim.api.nvim_get_current_buf()
+local keymap_options = { buffer = vim.api.nvim_get_current_buf(), silent = true }
 
-local function set_jdtls_keymap(modes, pattern, command)
-    vim.keymap.set(
-        modes,
-        pattern,
-        function() vim.cmd(('hor bo te %s/gradlew %s'):format(vim.fs.root(0, { '.git', 'gradlew' }), command)) end,
-        { buffer = buffer, silent = true }
-    )
+local function set_jdtls_keymap(pattern, command)
+    vim.keymap.set('n', pattern, function() command({}) end, keymap_options)
+    vim.keymap.set('v', pattern, function() command({ visual = true }) end, keymap_options)
 end
 
-set_jdtls_keymap('n', '<leader>rc', 'runClient')
-set_jdtls_keymap('n', '<leader>rs', 'runServer')
+set_jdtls_keymap('<leader>exv', function(options) require('jdtls').extract_variable(options) end)
+set_jdtls_keymap('<leader>exV', function(options) require('jdtls').extract_variable_all(options) end)
+set_jdtls_keymap('<leader>exc', function(options) require('jdtls').extract_constant(options) end)
+set_jdtls_keymap('<leader>exm', function(options) require('jdtls').extract_method(options) end)
+
+vim.keymap.set('n', '<leader>R', vim.cmd.JdtRestart, keymap_options)
+
+local function set_jdtls_gradle_keymap(modes, pattern, command)
+    local function run()
+        local root_path = vim.fs.root(0, { '.git', 'gradlew' })
+
+        vim.cmd(('hor bo te %s/gradlew %s'):format(root_path, command))
+    end
+
+    vim.keymap.set(modes, pattern, run, keymap_options)
+end
+
+set_jdtls_gradle_keymap('n', '<leader>gs', 'genSources')
+set_jdtls_gradle_keymap('n', '<leader>rc', 'runClient')
+set_jdtls_gradle_keymap('n', '<leader>rs', 'runServer')
--- configs/nvim/lua/config/nvim-lsp-keybinds.lua
+++ configs/nvim/lua/config/nvim-lsp-keybinds.lua
@@ -3,5 +3,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     callback = function(event)
         vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, { buffer = event.buf })
         vim.keymap.set('n', '<F4>', vim.lsp.buf.code_action, { buffer = event.buf })
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = event.buf })
+        vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, { buffer = event.buf })
     end,
 })
